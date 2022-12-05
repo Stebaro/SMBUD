af274ffb4ef6e5a71e09c2e54ecc5b4a5fe3dbde
Stefano <bs.baronistefano@gmail.com>
2022-12-06 00:20:35 +0100

    Added the creation/update queries

--- Prova.py
+++ Prova.py
@@ -1,6 +1,6 @@
 # Import the basic spark library
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import when, lit, collect_set, collect_list, col
+from pyspark.sql.functions import when, lit, collect_list, col, regexp_replace, size
 
 # Create an entry point to the PySpark Application
 spark = SparkSession.builder \
@@ -29,30 +29,73 @@ fos_df = spark.read.json("C:\\Users\\bsbar\\PycharmProjects\\pythonProject\\fos.
 publication_df = spark.read.json("C:\\Users\\bsbar\\PycharmProjects\\pythonProject\\publications.json")
 venue_df = spark.read.json("C:\\Users\\bsbar\\PycharmProjects\\pythonProject\\venues.json")
 rel_df = spark.read.json("C:\\Users\\bsbar\\PycharmProjects\\pythonProject\\rel_dw.json")
-#author_df.show()
-#fos_df.show()
+# author_df.show()
+# fos_df.show()
 #publication_df.show()
-#venue_df.show()
+# venue_df.show()
 #rel_df.show()
-creationalq = [False, False, True, False, False]
-normalq = [False, False, False, False, False, False, False, False, False, False]
+creationalq = [False, False, False, False, True]
+otherq = [False, False, False, False, False, False, False, False, False, False]
 
-#rdd=spark.sparkContext.parallelize(author_df)
-#"print(rdd.collect())
+# rdd=spark.sparkContext.parallelize(author_df)
+# "print(rdd.collect())
 
-# if (creationalq[1]):
-      #1 query creation of a row
 
-list=collect_list(author_df.affiliation)
-print(list)
+# CREATIONAL/UPDATE QUERIES:
 
-if (creationalq[2]):
-      #3 Creation of a new column that represents the region of the university
-      author_df = author_df.withColumn('Continent',
-                                       when(author_df["affiiliation"] in vectASIA) then lit( "Asia"),
-                                                                                        
-                                       (when(author_df["affiiliation"] in vectAMERICA, "America")),
-                                       (when(author_df["affiiliation"] in vectEU, "Europa"))
+if creationalq[0]:
+    # 1 query creation of a row
+    columns = ["date", "id", "number", "raw", "type", "volume"]
+    newRow = spark.createDataFrame([("2022-12-05", 9999999, 99, "New venue raw", "C", 3)], columns)
+    venue_df = venue_df.union(newRow)
+    venue_df.show(truncate=False)
 
-                                       )
-# if (creationalq[3]):
+if creationalq[1]:
+    # 2 Creation of the new column that counts the number of references
+    publication_df.withColumn("references number", size(col("references"))).show(truncate=False)
+
+if creationalq[2]:
+    # 3 Creation of a new column that represents the region of the university
+    author_df = author_df.withColumn('Continent',
+                                     when(author_df.affiliation.isin(vectASIA), lit("Asia"))
+                                     .when(author_df.affiliation.isin(vectAMERICA), lit("America"))
+                                     .when(author_df.affiliation.isin(vectEU), lit("Europa"))
+                                     .otherwise(lit("Rest of the world"))
+                                     ).show(truncate=False)
+
+if creationalq[3]:
+    # 4 Update of fos names
+    fos_df = fos_df.withColumn('name',
+                               when(fos_df.name.contains("mathematics"),
+                                    regexp_replace(fos_df.name, 'Discrete mathematics', 'Discrete Math'))
+                               .when(fos_df.name.contains("Mathematics"),
+                                     regexp_replace(fos_df.name, 'Mathematics', 'Math'))
+                               .when(fos_df.name.contains("Artificial intelligence"),
+                                     regexp_replace(fos_df.name, 'Artificial intelligence', 'AI'))
+                               .otherwise(fos_df.name)).show()
+
+if creationalq[4]:
+    # 5 Delete every publication whose type is Conference
+    publication_df = publication_df.where(publication_df.type != "Conference").show()
+
+
+# OTHER QUERIES:
+# if (otherq[0]):
+
+# if (otherq[1]):
+
+# if (otherq[2]):
+
+# if (otherq[3]):
+
+# if (otherq[4]):
+
+# if (otherq[5]):
+
+# if (otherq[6]):
+
+# if (otherq[7]):
+
+# if (otherq[8]):
+
+# if (otherq[9]):
