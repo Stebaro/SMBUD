5cc4d0dcf2a357db2aed3f3714f01522f067b0f4
Stefano <bs.baronistefano@gmail.com>
2022-12-06 00:20:42 +0100

    Changes by caterinagiardi

--- Prova.py
+++ Prova.py
@@ -4,23 +4,30 @@ from pyspark.sql.functions import when, lit, collect_list, col, regexp_replace, 
 
 # Create an entry point to the PySpark Application
 spark = SparkSession.builder \
-      .master("local") \
-      .appName("MyFirstSparkApplication") \
-      .getOrCreate()
+    .master("local") \
+    .appName("MyFirstSparkApplication") \
+    .getOrCreate()
 # master contains the URL of your remote spark instance or 'local'
 
-vectASIA= ["Department of Statistics, Rajshahi University, Rajshahi, Bangladesh",
-             "Department of Computer Science & Engineering, BUET, Dhaka, Bangladesh",
-             'Department of System Design and Informatics, Kyushu Institute of Technology, Fukuka, Japan',
-             "Corporate Research and Development Center, Toshiba Corporation, Kawasaki, Japan",
-             "Institute of Industrial Science, University of Tokyo, Meguro-Ku, Japan", "continent",
-             "Department of Human and Artificial Intelligence System, Graduate School of Engineering, University of Fukui, Japan",
-             "Department of Computer Science, Faculty of Engineering, Yamanashi University, Kofu, Japan", "Nippon Hoso Kyokai","Shinshu University",'Nagano Prefectural College' ]
+vectASIA = ["Department of Statistics, Rajshahi University, Rajshahi, Bangladesh",
+            "Department of Computer Science & Engineering, BUET, Dhaka, Bangladesh",
+            'Department of System Design and Informatics, Kyushu Institute of Technology, Fukuka, Japan',
+            "Corporate Research and Development Center, Toshiba Corporation, Kawasaki, Japan",
+            "Institute of Industrial Science, University of Tokyo, Meguro-Ku, Japan", "continent",
+            "Department of Human and Artificial Intelligence System, Graduate School of Engineering, University of Fukui, Japan",
+            "Department of Computer Science, Faculty of Engineering, Yamanashi University, Kofu, Japan",
+            "Nippon Hoso Kyokai", "Shinshu University", 'Nagano Prefectural College']
 
-vectEU = ["Università della Calabria","Università degli Studi di Modena e Reggio Emilia","Politecnico di Milano", 'Università degli Studi di Firenze'
-          "University of Milano-Bicocca", "Cognitive Interaction Technology, Center of Excellence and Applied Informatics, Faculty of Technology, Bielefeld University, Bielefeld, Germany#TAB#", "Department of Information Systems and Business Administration, Johannes Gutenberg-Universität Mainz, Jakob-Welder-Weg 9, Mainz 55128, Germany",
-          "Dept. of Computer Science, Uni. Politècnica de Catalunya, Spain#TAB#","university of malaga", "Computer Laboratory, University Of Cambridge", "New England Aquarium", "Unconventional Computing Group, University of the West of England, Bristol, UK BS16 1QY#TAB#",
-          "ESIEE-Paris","UNIVERSITÉ LAVAL", "University of Southern Denmark","University of Lisbon","#N#University of Glasgow#N#"]
+vectEU = ["Università della Calabria", "Università degli Studi di Modena e Reggio Emilia", "Politecnico di Milano",
+          'Università degli Studi di Firenze'
+          "University of Milano-Bicocca",
+          "Cognitive Interaction Technology, Center of Excellence and Applied Informatics, Faculty of Technology, Bielefeld University, Bielefeld, Germany#TAB#",
+          "Department of Information Systems and Business Administration, Johannes Gutenberg-Universität Mainz, Jakob-Welder-Weg 9, Mainz 55128, Germany",
+          "Dept. of Computer Science, Uni. Politècnica de Catalunya, Spain#TAB#", "university of malaga",
+          "Computer Laboratory, University Of Cambridge", "New England Aquarium",
+          "Unconventional Computing Group, University of the West of England, Bristol, UK BS16 1QY#TAB#",
+          "ESIEE-Paris", "UNIVERSITÉ LAVAL", "University of Southern Denmark", "University of Lisbon",
+          "#N#University of Glasgow#N#"]
 
 vectAMERICA = ['The University of Chicago Press, Chicago, USA']
 
